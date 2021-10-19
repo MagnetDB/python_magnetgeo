600608b68ca10befae6b9699f584b7e8f9c56ede
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2021-10-19 11:55:29 +0200

    Fix from R. Vallet

--- python_magnetgeo/python_magnetgeo.py
+++ python_magnetgeo/python_magnetgeo.py
@@ -2,6 +2,8 @@
 
 from .Insert import *
 
+import math
+
 def get_main_characteristics(cad: Insert):
     """
     return main characteristics for Insert
@@ -25,6 +27,12 @@ def get_main_characteristics(cad: Insert):
     R2 = []
     Z1 = []
     Z2 = []
+    Nsections = []
+    index_h = []
+    Zmin = [] 
+    Zmax =  []
+    Dh = []
+    Sh = []
     for i,helix in enumerate(cad.Helices):
         hhelix = None
         with open(helix+".yaml", 'r') as f:
@@ -38,28 +46,28 @@ def get_main_characteristics(cad: Insert):
         Z1.append(hhelix.z[0])
         Z2.append(hhelix.z[1])
 
-        Ri = cad.innerbore
-        Re = cad.outerbore
+    Ri = cad.innerbore
+    Re = cad.outerbore
         
-        zm1 = Z1[0]
-        zm2 = Z2[0]
+    zm1 = Z1[0]
+    zm2 = Z2[0]
             
-        for i in range(NHelices):
+    for i in range(NHelices):
 
-            Zmin.append(min(Z1[i],zm1))
-            Zmax.append(min(Z2[i],zm2))
+        Zmin.append(min(Z1[i],zm1))
+        Zmax.append(min(Z2[i],zm2))
 
-            Dh.append(2*(R1[i]-Ri))
-            Sh.append(math.pi*(R1[i]-Ri)*(R1[i]+Ri))
+        Dh.append(2*(R1[i]-Ri))
+        Sh.append(math.pi*(R1[i]-Ri)*(R1[i]+Ri))
             
-            Ri = R1[i]
-            zm1 = Z1[i]
-            zm2 = Z2[i]
+        Ri = R1[i]
+        zm1 = Z1[i]
+        zm2 = Z2[i]
 
-        Zmin.append(zm1)
-        Zmax.append(zm2)
+    Zmin.append(zm1)
+    Zmax.append(zm2)
 
-        Dh.append(2*(Re-Ri))
-        Sh.append(math.pi*(Re-Ri)*(Re+Ri))
+    Dh.append(2*(Re-Ri))
+    Sh.append(math.pi*(Re-Ri)*(Re+Ri))
 
     return (NHelices, NRings, NChannels, Nsections, index_h, R1, R2, Z1, Z2, Zmin, Zmax, Dh, Sh)
