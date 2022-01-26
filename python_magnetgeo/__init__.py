a7aee36158b919c609be59ee5d4a913fc1c7d5f6
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2022-01-26 10:53:37 +0100

    fix setup entry point

--- python_magnetgeo/__init__.py
+++ python_magnetgeo/__init__.py
@@ -4,4 +4,3 @@ __author__ = """Christophe Trophime"""
 __email__ = 'christophe.trophime@lncmi.cnrs.fr'
 __version__ = '0.1.0'
 
-from .python_magnetgeo import *
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ 'lxml', 'gmsh>=4.8.4', 'yaml', ]
+requirements = [ 'lxml', 'gmsh>=4.8.4', 'pyyaml', ]
 
 setup_requirements = ['pytest-runner', ]
 
