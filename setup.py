13990f4099dd5c62ab907e2e2dd99c38c6f696c6
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2022-01-26 11:56:19 +0100

    remove pytest-runner from build requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst') as history_file:
 
 requirements = [ 'lxml', 'gmsh>=4.8.4', 'pyyaml', ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [ ]
 
 test_requirements = ['pytest>=3', ]
 
