c508dae2e4002bea1dccbeb61ee6afc14cb269cc
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2023-02-09 14:13:19 +0100

    avoid magnetsetup dependence

--- python_magnetgeo/python_magnetgeo.py
+++ python_magnetgeo/python_magnetgeo.py
@@ -3,47 +3,41 @@
 from .Insert import *
 
 import math
-import os
 
-from python_magnetsetup.file_utils import MyOpen, search_paths
 
-def get_main_characteristics(cad: Insert, MyEnv = None ):
+def get_main_characteristics(cad: Insert, workingDir: str = ".") -> tuple:
     """
     return main characteristics for Insert
     NHelices
     NRings
-    NChannels 
+    NChannels
     R1
     R2
     Z1
     Z2
-    Zmin, 
-    Zmax, 
-    Dh, 
+    Zmin,
+    Zmax,
+    Dh,
     Sh,
     """
-    
+
     NHelices = len(cad.Helices)
     NRings = len(cad.Rings)
-    NChannels = NHelices+1 # TODO check this value if insert contains HR 
+    NChannels = NHelices + 1  # TODO check this value if insert contains HR
     R1 = []
     R2 = []
     Z1 = []
     Z2 = []
     Nsections = []
     Nturns_h = []
-    Zmin = [] 
-    Zmax =  []
+    Zmin = []
+    Zmax = []
     Dh = []
     Sh = []
-    for i,helix in enumerate(cad.Helices):
+    for i, helix in enumerate(cad.Helices):
         hhelix = None
-        if MyEnv:
-            with MyOpen(helix+".yaml", 'r', paths=search_paths(MyEnv, "geom")) as f:
-                hhelix = yaml.load(f, Loader = yaml.FullLoader)
-        else:
-            with open(helix+".yaml", 'r') as f:
-                hhelix = yaml.load(f, Loader = yaml.FullLoader)
+        with open(f"{workingDir}/{helix}.yaml", "r") as f:
+            hhelix = yaml.load(f, Loader=yaml.FullLoader)
         n_sections = len(hhelix.axi.turns)
         Nsections.append(n_sections)
         Nturns_h.append(hhelix.axi.turns)
@@ -55,18 +49,18 @@ def get_main_characteristics(cad: Insert, MyEnv = None ):
 
     Ri = cad.innerbore
     Re = cad.outerbore
-        
+
     zm1 = Z1[0]
     zm2 = Z2[0]
-            
+
     for i in range(NHelices):
 
-        Zmin.append(min(Z1[i],zm1))
-        Zmax.append(min(Z2[i],zm2))
+        Zmin.append(min(Z1[i], zm1))
+        Zmax.append(min(Z2[i], zm2))
 
-        Dh.append(2*(R1[i]-Ri))
-        Sh.append(math.pi*(R1[i]-Ri)*(R1[i]+Ri))
-            
+        Dh.append(2 * (R1[i] - Ri))
+        Sh.append(math.pi * (R1[i] - Ri) * (R1[i] + Ri))
+
         Ri = R1[i]
         zm1 = Z1[i]
         zm2 = Z2[i]
@@ -74,30 +68,27 @@ def get_main_characteristics(cad: Insert, MyEnv = None ):
     Zmin.append(zm1)
     Zmax.append(zm2)
 
-    Dh.append(2*(Re-Ri))
-    Sh.append(math.pi*(Re-Ri)*(Re+Ri))
+    Dh.append(2 * (Re - Ri))
+    Sh.append(math.pi * (Re - Ri) * (Re + Ri))
 
     return (NHelices, NRings, NChannels, Nsections, R1, R2, Z1, Z2, Zmin, Zmax, Dh, Sh)
 
-def get_cut_characteristics(cad: Insert, MyEnv = None):
+
+def get_cut_characteristics(cad: Insert, workingDir: str = ".") -> tuple:
     """
     return cut characteristics for Insert
     Nturns
     NPitch
     """
-    
+
     NHelices = len(cad.Helices)
     Nturns = []
-    Pitch = [] 
-    for i,helix in enumerate(cad.Helices):
+    Pitch = []
+    for i, helix in enumerate(cad.Helices):
         hhelix = None
-        if MyEnv:
-            with MyOpen(helix+".yaml", 'r', paths=search_paths(MyEnv, "geom")) as f:
-                hhelix = yaml.load(f, Loader = yaml.FullLoader)
-        else:
-            with open(helix+".yaml", 'r') as f:
-                hhelix = yaml.load(f, Loader = yaml.FullLoader)
+        with open(f"{workingDir}/{helix}.yaml", "r") as f:
+            hhelix = yaml.load(f, Loader=yaml.FullLoader)
         Nturns.append(hhelix.axi.turns)
         Pitch.append(hhelix.axi.pitch)
-    
+
     return (Nturns, Pitch)
