76f60df19251f267251509056faa2bffaa120ad8
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2025-06-11 17:19:03 +0200

    add dr param

--- python_magnetgeo/Chamfer.py
+++ python_magnetgeo/Chamfer.py
@@ -22,19 +22,34 @@ class Chamfer(yaml.YAMLObject):
 
     params :
       side: BP or HP
-      rside: rint or rext
+      rside: rint or rext in mm
       alpha: angle in degree
+      dr: shift along r in mm
       L: height in mm
     """
 
     yaml_tag = "Chamfer"
 
+    def __setstate__(self, state):
+        """
+        This method is called during deserialization (when loading from YAML or pickle)
+        We use it to ensure the optional attributes always exist
+        """
+        self.__dict__.update(state)
+        
+        # Ensure these attributes always exist
+        if not hasattr(self, 'dr'):
+            self.dr = None
+        if not hasattr(self, 'alpha'):
+            self.alpha = None
+
     def __init__(
         self,
         side: str,
         rside: str,
-        alpha: float,
-        L: float,
+        alpha: float = None,
+        dr: float = None,
+        L: float = None,
     ):
         """
         initialize object
@@ -42,19 +57,22 @@ class Chamfer(yaml.YAMLObject):
         self.side = side
         self.rside = rside
         self.alpha = alpha
+        self.dr = dr
         self.L = L
 
     def __repr__(self):
         """
         representation of object
         """
-        return "%s(side=%s, rside=%s, alpha=%g, L=%g)" % (
-            self.__class__.__name__,
-            self.side,
-            self.rside,
-            self.alpha,
-            self.L,
-        )
+        msg = self.__class__.__name__
+        msg += f"(side={self.side}, "
+        msg += f", rside={self.rside}"
+        if hasattr(self, "alpha"):
+            msg += f", alpha={self.alpha}}"
+        if hasattr(self, "dr"):
+            msg += f", dr={self.dr}}"
+        msg += f", L=self.{L})"
+        return msg
 
     def dump(self, name: str):
         """
@@ -79,7 +97,12 @@ class Chamfer(yaml.YAMLObject):
 
         self.side = data.side
         self.rside = data.rside
-        self.alpha = data.alpha
+
+        # Always ensure these attributes exist for backward compatibility
+        # Even if they don't exist in the loaded data
+        self.alpha = getattr(data, 'alpha', None)
+        self.dr = getattr(data, 'dr', None)
+
         self.L = data.L
 
     def to_json(self):
@@ -110,12 +133,25 @@ class Chamfer(yaml.YAMLObject):
         """
         returns chamfer radius reduction 
         """
+        if self.dr:
+            return self.dr
+
         radius = (
                 self.L
                 * math.tan(math.pi / 180.0 * self.alpha)
             )
         return radius
 
+    def getAngle(self):
+        """
+        returns chamfer angle 
+        """
+        if self.alpha:
+            return self.alpha
+
+        angle = math.atan2(dr, L)
+        return angle * 180 / math.pi
+    
 
 def Chamfer_constructor(loader, node):
     """
@@ -124,7 +160,11 @@ def Chamfer_constructor(loader, node):
     values = loader.construct_mapping(node)
     side = values["side"]
     rside = values["rside"]
-    alpha = values["alpha"]
+
+    # Make chamfers and grooves optional
+    alpha = values.get("alpha", None)
+    dr = values.get("dr", None)
+
     L = values["L"]
     return Chamfer(side, rside, alpha, L)
 
